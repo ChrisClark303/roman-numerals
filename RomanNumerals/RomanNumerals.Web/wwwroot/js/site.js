644582f5df5139521b8f56e9b0caa45231950307
ChrisClark303 <38655477+ChrisClark303@users.noreply.github.com>
2022-02-22 11:51:18 +0000

    Added an API and a web project to consume it

---
+++ RomanNumerals/RomanNumerals.Web/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+var error;
+$("#parse").click(function () {
+    $("#error").hide();
+    var numerals = $("#romanNumerals").val();
+    var jqxhr = $.get("http://localhost:49101/parse/" + numerals, function (data) {
+        $("#result").text(numerals + " = " + data);
+    })
+        .done(function () {
+            //alert("second success");
+        })
+        .fail(function (data) {
+            error = data.responseJSON.detail;
+            $("#error").show();
+            $("#error").text(error);
+        });
+});
